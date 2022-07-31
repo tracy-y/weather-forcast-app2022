dc66ba937a3cd5aa69b485c7d38e381b632b8718
Tracey <tracy.yan2018@gmail.com>
2022-07-31 15:17:41 +1000

    Background image& App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import styled from 'styled-components';
 
 const BackgroundImage = styled.div`
   min-height: 100vh;
-  min-width: 100vh;
-  background-color: green;
-  background-image: url(https://wallpaperaccess.com/download/hd-51367);
+  min-width: 100vw;
+  background-image: url(../public/51367.jpg);
   background-size:cover
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 `
-
+const Container = styled.div`
+  border-radius: 32px;
+  box-shadow: 0 0 16px rgb(0 0 0 / 50%)
+  background-color: white;
+  width: 80px;
+`
 const App= () => (
-  <BackgroundImage></BackgroundImage>
+  <BackgroundImage>
+    <Container>
+      Weather App
+    </Container>
+  </BackgroundImage>
 )
 export default App;
