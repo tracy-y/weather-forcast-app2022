b956c7d64cabc406b1659bcba8e4d9c78a63a219
Tracey <67274280+tracy-y@users.noreply.github.com>
2022-10-05 22:04:42 +1100

    Update getWeather.js

--- src/apis/getWeather/getWeather.js
+++ src/apis/getWeather/getWeather.js
@@ -1,5 +1,7 @@
 import OpenWeatherMap from '../../libs/OpenWeatherMap/OpenWeatherMap';
 
+
+// get api from a path.
 const getWeather = (id) => OpenWeatherMap.get({
   path: '/weather',
   params: {
